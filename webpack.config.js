4f0d0ab8cf747e5a1de18371bcdc7a54e33a8317
simolex <simolex@yandex.ru>
2023-01-29 23:09:13 +0300

    create index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
     mode: "development",
     entry: path.resolve(__dirname, "src", "index.js"),
     output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "build")
-    }
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "build"),
+        clean: true
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        })
+    ]
 };
