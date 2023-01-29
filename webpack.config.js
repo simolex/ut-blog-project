fe72cd2d0b47e7f0d25286697ea37a96e4ba86b0
simolex <simolex@yandex.ru>
2023-01-29 23:19:40 +0300

    rewrite config to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,23 @@ const webpack = require("webpack");
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, "src", "index.js"),
+    entry: path.resolve(__dirname, "src", "index.ts"),
     output: {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "build"),
         clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
     },
     plugins: [
         new webpack.ProgressPlugin(),
