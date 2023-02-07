24f7abcfd81e000b93669da3a533003eee4cae47
simolex <simolex@yandex.ru>
2023-02-07 14:54:08 +0300

    replace ButtonSwitcher to shared

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,8 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher/ThemeSwitcher";
+
 import styles from "./Navbar.module.scss";
 
 interface NavbarProps {
