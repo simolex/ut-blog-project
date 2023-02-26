1f077c7d0e06085356d59ad67be8f77e6d62119c
simolex <simolex@yandex.ru>
2023-02-26 19:51:22 +0300

    fix: Navbar

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 
 import { Navbar } from './Navbar';
@@ -16,8 +17,13 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [StoreDecorator({})];
+
+export const LightAuth = Template.bind({});
+LightAuth.args = {};
+LightAuth.decorators = [StoreDecorator({ user: { authDate: {} } })];
 
 export const Dark = Template.bind({});
 Dark.args = {};
 
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
