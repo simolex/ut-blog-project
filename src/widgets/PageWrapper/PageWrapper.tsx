1bae6165ab56c7db2ca033160d6444cfeb1dee01
simolex <simolex@yandex.ru>
2023-04-03 17:26:29 +0300

    add virtual listing for type List

--- src/widgets/PageWrapper/PageWrapper.tsx
+++ src/widgets/PageWrapper/PageWrapper.tsx
@@ -16,6 +16,8 @@ interface PageWrapperProps {
     onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const PageWrapper = (props: PageWrapperProps) => {
     const { className, children, onScrollEnd } = props;
     const wrapperRef = useRef() as MutableRefObject<HTMLElement>;
@@ -51,6 +53,7 @@ export const PageWrapper = (props: PageWrapperProps) => {
             ref={wrapperRef}
             className={classNames(styles.pageWrapper, {}, [className])}
             onScroll={onScroll}
+            id={PAGE_ID}
         >
             {children}
             {onScrollEnd ? <div ref={triggerRef} className={styles.trigger} /> : null}
