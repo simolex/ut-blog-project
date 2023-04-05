2537f2479f8104a9a6f1e6baff6ec0ec11324512
simolex <simolex@yandex.ru>
2023-04-05 15:16:41 +0300

    move ThemeSwitcher to featches

---
+++ src/features/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+import { memo } from 'react';
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+
+interface ThemeSwitcherProps {
+    className?: string;
+}
+
+export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
+    const { theme, toggleTheme } = useTheme();
+    return (
+        <Button
+            theme={ButtonTheme.CLEAR}
+            onClick={toggleTheme}
+            className={classNames('', {}, [className])}
+        >
+            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+        </Button>
+    );
+});
