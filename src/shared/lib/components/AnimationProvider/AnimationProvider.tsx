5c6792a446ac3eef2ca6c7b5650fb14772b6587d
simolex <simolex@yandex.ru>
2023-04-18 17:55:56 +0300

    add lazy loading function for libs

--- src/shared/lib/components/AnimationProvider/AnimationProvider.tsx
+++ src/shared/lib/components/AnimationProvider/AnimationProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, ReactNode, useEffect, useMemo, useRef, useState } from 'react';
 
 type SpringType = typeof import('@react-spring/web');
 type GestureType = typeof import('@use-gesture/react');
@@ -9,4 +9,33 @@ interface AnimationContextPayload {
     isLoaded?: boolean;
 }
 
-const AnimationContext = createContext('');
+const AnimationContext = createContext<AnimationContextPayload>({});
+
+// Обе библиотеке используются совместно. Грузить нужно одновременно.
+const getLazyAnimationModules = () =>
+    Promise.all([import('@react-spring/web'), import('@use-gesture/react')]);
+
+export const AnimationProvider = ({ children }: { children: ReactNode }) => {
+    const SpringRef = useRef<SpringType>();
+    const GestureRef = useRef<GestureType>();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        getLazyAnimationModules().then(([Spring, Gesture]) => {
+            SpringRef.current = Spring;
+            GestureRef.current = Gesture;
+            setIsLoaded(true);
+        });
+    }, []);
+
+    const value = useMemo(
+        () => ({
+            Gesture: GestureRef.current,
+            Spring: SpringRef.current,
+            isLoaded,
+        }),
+        [isLoaded],
+    );
+
+    return <AnimationContext.Provider value={value}>{children}</AnimationContext.Provider>;
+};
