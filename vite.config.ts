f8a518403869e62d76c89d7ec1db861b64f7a921
simolex <simolex@yandex.ru>
2023-04-19 13:00:47 +0300

    add Vite dev builder

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+import fs from 'fs';
+
+const WRONG_CODE = 'import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";';
+export function reactVirtualized() {
+    return {
+        name: 'my:react-virtualized',
+        configResolved() {
+            const file = require
+                .resolve('react-virtualized')
+                .replace(
+                    path.join('dist', 'commonjs', 'index.js'),
+                    path.join('dist', 'es', 'WindowScroller', 'utils', 'onScroll.js'),
+                );
+            const code = fs.readFileSync(file, 'utf-8');
+            const modified = code.replace(WRONG_CODE, '');
+            fs.writeFileSync(file, modified);
+        },
+    };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [reactVirtualized(), svgr({ exportAsDefault: true }), react()],
+    resolve: { alias: [{ find: '@', replacement: '/src' }] },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('http://localhost:7000'),
+        __PROJECT__: JSON.stringify('frontend'),
+    },
+});
