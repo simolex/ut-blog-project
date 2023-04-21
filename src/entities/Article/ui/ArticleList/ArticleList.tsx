ceab2553373b7301f94da1e73251ff9e48313e47
simolex <simolex@yandex.ru>
2023-04-22 00:13:25 +0300

    install Virtuoso & basic apply

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,6 +1,14 @@
-import { HTMLAttributeAnchorTarget, memo } from 'react';
+import {
+    ForwardedRef,
+    forwardRef,
+    HTMLAttributeAnchorTarget,
+    LegacyRef,
+    memo,
+    useRef,
+} from 'react';
 import { useTranslation } from 'react-i18next';
-import { List, ListRowProps, WindowScroller } from 'react-virtualized';
+// import { List, ListRowProps, WindowScroller } from 'react-virtualized';
+import { GridItemProps, GridListProps, VirtuosoGrid, VirtuosoGridHandle } from 'react-virtuoso';
 import { classNames } from '@/shared/lib/classNames';
 import { Text } from '@/shared/ui/Text/Text';
 import { Article, ArticleView } from '../../model/types/article';
@@ -33,32 +41,34 @@ export const ArticleList = memo((props: ArticleListProps) => {
     } = props;
     const { t } = useTranslation('article');
 
+    const ref = useRef<VirtuosoGridHandle>(null);
+
     const isList = view === ArticleView.LIST;
     const itemsPerRow = isList ? 1 : 3;
     const rowCount = isList ? articles.length : Math.ceil(articles.length / itemsPerRow);
 
-    const rowRenderer = ({ index, isScrolling, isVisible, key, style }: ListRowProps) => {
-        const items = [];
-        const fromIndex = index * itemsPerRow;
-        const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
+    // const rowRenderer = ({ index, isScrolling, isVisible, key, style }: ListRowProps) => {
+    //     const items = [];
+    //     const fromIndex = index * itemsPerRow;
+    //     const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
 
-        for (let i = fromIndex; i < toIndex; i += 1) {
-            items.push(
-                <ArticleListItem
-                    article={articles[i]}
-                    view={view}
-                    target={target}
-                    key={articles[i].id}
-                />,
-            );
-        }
+    //     for (let i = fromIndex; i < toIndex; i += 1) {
+    //         items.push(
+    //             <ArticleListItem
+    //                 article={articles[i]}
+    //                 view={view}
+    //                 target={target}
+    //                 key={articles[i].id}
+    //             />,
+    //         );
+    //     }
 
-        return (
-            <div key={key} className={styles.rowList} style={style}>
-                {items}
-            </div>
-        );
-    };
+    //     return (
+    //         <div key={key} className={styles.rowList} style={style}>
+    //             {items}
+    //         </div>
+    //     );
+    // };
 
     // const renderArticles = (article: Article) => (
     //     <ArticleListItem article={article} view={view} key={article.id} target={target} />
@@ -74,39 +84,68 @@ export const ArticleList = memo((props: ArticleListProps) => {
 
     return (
         // @ts-ignore
-        <WindowScroller scrollElement={document.getElementById('PAGE_ID') as Element}>
-            {({ width, height, registerChild, onChildScroll, isScrolling, scrollTop }) => (
-                <div
-                    className={classNames(styles.articleList, {}, [className])}
-                    // @ts-ignore
-                    ref={registerChild}
-                >
-                    {virtualized ? (
-                        // @ts-ignore
-                        <List
-                            height={height ?? 700}
-                            rowCount={rowCount}
-                            rowHeight={isList ? 700 : 330}
-                            rowRenderer={rowRenderer}
-                            width={width ? width - 60 : 700}
-                            autoHeight
-                            isScrolling={isScrolling}
-                            onScroll={onChildScroll}
-                            scrollTop={scrollTop}
-                        />
-                    ) : (
-                        articles.map((item) => (
-                            <ArticleListItem
-                                article={item}
-                                view={view}
-                                target={target}
-                                key={item.id}
-                            />
-                        ))
-                    )}
-                    {isLoading && getSkeleton(view)}
-                </div>
+        <VirtuosoGrid
+            ref={ref}
+            totalCount={articles?.length}
+            style={{ height: '100%' }}
+            customScrollParent={document.getElementById('PAGE_ID') as HTMLElement}
+            useWindowScroll
+            itemContent={(index) => (
+                <ArticleListItem
+                    article={articles[index]}
+                    view={view}
+                    target={target}
+                    key={articles[index].id}
+                />
             )}
-        </WindowScroller>
+            components={{
+                List: forwardRef(
+                    ({ style, children }: GridListProps, listRef: ForwardedRef<HTMLDivElement>) => (
+                        <div
+                            style={{ ...style }}
+                            className={classNames(styles.articleList, {}, [className])}
+                            // @ts-ignore
+                            ref={listRef}
+                        >
+                            {children}
+                        </div>
+                    ),
+                ),
+            }}
+        />
+        // <WindowScroller scrollElement={document.getElementById('PAGE_ID') as Element}>
+        //     {({ width, height, registerChild, onChildScroll, isScrolling, scrollTop }) => (
+        //         <div
+        //             className={classNames(styles.articleList, {}, [className])}
+        //             // @ts-ignore
+        //             ref={registerChild}
+        //         >
+        //             {virtualized ? (
+        //                 // @ts-ignore
+        //                 <List
+        //                     height={height ?? 700}
+        //                     rowCount={rowCount}
+        //                     rowHeight={isList ? 700 : 330}
+        //                     rowRenderer={rowRenderer}
+        //                     width={width ? width - 60 : 700}
+        //                     autoHeight
+        //                     isScrolling={isScrolling}
+        //                     onScroll={onChildScroll}
+        //                     scrollTop={scrollTop}
+        //                 />
+        //             ) : (
+        //                 articles.map((item) => (
+        //                     <ArticleListItem
+        //                         article={item}
+        //                         view={view}
+        //                         target={target}
+        //                         key={item.id}
+        //                     />
+        //                 ))
+        //             )}
+        //             {isLoading && getSkeleton(view)}
+        //         </div>
+        //     )}
+        // </WindowScroller>
     );
 });
