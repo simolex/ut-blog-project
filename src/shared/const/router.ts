521d08c9aacebb66da853bbb5a7ef666216b5290
simolex <simolex@yandex.ru>
2023-04-28 15:57:30 +0300

    little resort

--- src/shared/const/router.ts
+++ src/shared/const/router.ts
@@ -13,10 +13,10 @@ export enum AppRoutes {
 
 export const getRouteMain = () => '/';
 export const getRouteAbout = () => '/about';
-export const getRouteProfile = (id: string) => `/profile/${id}`;
-export const getRouteArticles = () => '/articles';
-export const getRouteArticleDetails = (id: string) => `/articles/${id}`;
-export const getRouteArticleCreate = () => '/articles/add';
-export const getRouteArticleEdit = (id: string) => `/articles/${id}/edit`;
 export const getRouteAdminPanel = () => '/admin';
+export const getRouteArticles = () => '/articles';
 export const getRouteForbidden = () => '/forbidden';
+export const getRouteArticleCreate = () => '/articles/add';
+export const getRouteProfile = (id: string) => `/profile/${id}`;
+export const getRouteArticleDetails = (id: string) => `/articles/${id}`;
+export const getRouteArticleEdit = (id: string) => `/articles/${id}/edit`;
