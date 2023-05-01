18190c5a853515741f3f07302628557a574c0763
simolex <simolex@yandex.ru>
2023-05-01 13:41:16 +0300

    add helper functions for slice and selectors

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,10 +1,16 @@
 import { FC, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PageWrapper } from '@/widgets/PageWrapper';
+import { Counter } from '@/entities/Counter';
 
 const AboutPage: FC = () => {
     const { t } = useTranslation('about');
-    return <PageWrapper>{t('О сайте')}</PageWrapper>;
+    return (
+        <PageWrapper>
+            {t('О сайте')}
+            <Counter />
+        </PageWrapper>
+    );
 };
 
 export default memo(AboutPage);
